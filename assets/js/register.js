92473d9404adf5c87f0821ccdac93970f7fc4dc5
Agathiyan V <agathi.nv@gmail.com>
2021-09-03 18:48:52 +0530

    less concluded project added

---
+++ assets/js/register.js
@@ -0,0 +1,64 @@
+$("#header").load("_header.html");
+
+
+
+function register() {
+
+    event.preventDefault();
+    const name = document.querySelector("#registerName").value;
+
+    const email = document.querySelector("#registerEmail").value.toLowerCase();
+    const password = document.querySelector("#registerPassword").value;
+    const confirmPassword = document.querySelector("#registerConfirmPassword").value;
+
+    try {
+
+
+
+
+        RegisterValidator.validate(name, email, password, confirmPassword)
+
+        const registerObj = {
+            "name": name,
+            "email": email,
+            "password": password,
+            "role": "USER"
+        }
+        isEmailExists(email).then(res => {
+
+                let exists = res;
+                if (exists) {
+                    toastr.error("", "This email Id is already exists", //if its true it is an error
+                        {
+                            preventDuplicate: true
+                        });
+                } else {
+
+                    console.log(registerObj);
+                    UserService.register(registerObj).then(res => {
+                        let data = res.data;
+                        console.log(data);
+                        toastr.success("successfully register");
+                        setTimeout(function() {
+                            window.location.href = "home_login.html";
+                        }, 1000)
+
+
+                    })
+
+                    .catch(err => {
+                        console.log(err.response.data);
+                        toastr.error("Unable to Register");
+                    });
+                }
+            })
+            .catch(err => {
+
+                console.error(err);
+                toastr.error("Error" + err.message)
+            });
+    } catch (err) {
+        console.log(err);
+        toastr.error(err.message);
+    }
+}
